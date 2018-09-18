1c851cecc7d16c4483ad9391e064b02b4408af11
atabak <atabak_kh@yahoo.com>
2018-09-18 15:36:04 +0800

    first deployment

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask
 app = Flask(__name__) 
 cache = redis.Redis(host='redis', port=6379) 
 
+# get the number of hit
 def get_hit_count(): 
         retries = 5 
         while True: 
